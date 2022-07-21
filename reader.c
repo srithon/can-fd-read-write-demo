8431f1fe006516235ecd31e2a81e6cecd4d12898
Sridaran Thoniyil <sri7thon@gmail.com>
2022-07-21 00:50:34 -0400

    Update reader to handle CAN-FD frames.

--- reader.c
+++ reader.c
@@ -1,4 +1,6 @@
 #include <linux/can.h>
+// for access to CAN FD
+#include <linux/can/raw.h>
 
 // contains ifreq
 // MISTAKE: included linux/if.h rather than net/if.h
@@ -21,15 +23,15 @@
 #define CAN_INTERFACE_NAME "vcan0"
 
 void read_from_socket(int s) {
-  struct can_frame frame;
+  struct canfd_frame frame;
   int nbytes = read(s, &frame, sizeof(frame));
 
   if (nbytes < 0) {
-    perror("CAN Raw socket read");
+    perror("CAN FD Raw socket read");
   }
 
   if (nbytes < sizeof(frame)) {
-    perror("read: Incomplete CAN frame");
+    perror("read: Incomplete CAN FD frame");
   }
 
   printf("CAN ID: %d\n", frame.can_id);
@@ -40,12 +42,16 @@ void read_from_socket(int s) {
 int main() {
   // pre-declaration for C90 compatibility
   int s;
+  int canfd_enabled = 1;
   struct sockaddr_can addr;
   // structure for requesting a specific interface
   struct ifreq ifr;
 
   // open socket communicating using the raw socket protocol
   s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+  // https://stackoverflow.com/questions/61368853/socketcan-read-function-never-returns
+  setsockopt(s, SOL_CAN_RAW, CAN_RAW_FD_FRAMES, &canfd_enabled,
+             sizeof(canfd_enabled));
 
   // we copy the name of our device into the struct.
   // note that `ifr_name` is actually a macro that hides the internal structure
@@ -64,7 +70,9 @@ int main() {
   // TODO: figure out this line
   bind(s, (struct socketaddr *)&addr, sizeof(addr));
 
-  read_from_socket(s);
+  while (1) {
+    read_from_socket(s);
+  }
 
   return 0;
 }
